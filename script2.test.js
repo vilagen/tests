fb785f374d441b82dda41e45935a3291508459ec
vilagen <vilagen@yahoo.com>
2020-01-04 09:36:47 -0500

    Wrote second test.

---
+++ script2.test.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+it('calls swapi to get people', (done) => { //using a callback, like done, to tell jest to wait for done callback to be called before finishing test.
+  expect.assertions(1)
+  swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    done();
+  });
+});
+
+it('calls swapi to get people using return', () => {
+  expect.assertions(1) // tip, always test assertions to when using async to make sure that assertions are being tested.
+  return swapi.getPeople(fetch).then(data => { // omitting return will make test complete before it is resolved or rejected, making it look like it pasts.
+    expect(data.count).toEqual(87);
+  });
+});
+
+it('calls swapi to get people with a promise', () => {
+  expect.assertions(2)
+  return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
+
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn()
+  .mockReturnValue(Promise.resolve({
+    json: () =>Promise.resolve({
+      count:87,
+      results: [0,1,2,3,4,5]
+    })
+  }))
+
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch) .then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  })
+});
